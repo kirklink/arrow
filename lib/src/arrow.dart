78cb69ca30c441f9e682d7e0f97795436c0f0f02
Kirk Linkletter <kirklinkletter@gmail.com>
2019-09-25 02:50:04 +0000

    doc updates

--- lib/src/arrow.dart
+++ lib/src/arrow.dart
@@ -6,10 +6,12 @@ import 'server.dart';
 /// An Arrow server.
 class Arrow {
 
-/// Starts the Arrow server with the provided [Router]. The [port] can be specified here but will be
-/// overrode if the ARROW_PORT environment variable is set. Setting [forceSSL]
-/// to true will redirect all http requests to https. [printRoutes] prints all
-/// the configured routes to stdout when the server starts.
+/// Starts the Arrow server with the provided [Router]. 
+/// 
+/// The [port] can be specified here but the ARROW_PORT environment variable
+/// will override this value if ARROW_PORT is found in the environment. Setting 
+/// [forceSSL] to true will redirect all http requests to https. [printRoutes] prints 
+/// all the configured routes to stdout when the server starts.
   Future run(Router router,
       {int port: 8080,
       bool forceSSL: false,
