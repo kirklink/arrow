38fe0148569aee3c5ae1bb2c2fecb27f9500f596
Kirk Linkletter <kirklinkletter@gmail.com>
2019-09-25 02:24:20 +0000

    more docs

--- lib/src/arrow.dart
+++ lib/src/arrow.dart
@@ -3,31 +3,25 @@ import 'dart:async';
 import 'router.dart';
 import 'server.dart';
 
-/// The main entry point to starting an Arrow server. Create an instance
-/// and start it with [Arrow.run()].
+/// An Arrow server.
 class Arrow {
-  RouterBuilder _routerBuilder;
 
-/// Arrow is constructed with a [RouterBuilder] which provides the route 
-/// configurations to the Arrow server.
-  Arrow(this._routerBuilder);
-
-/// Starts the Arrow server. The [port] can be specified here but will be
+/// Starts the Arrow server with the provided [Router]. The [port] can be specified here but will be
 /// overrode if the ARROW_PORT environment variable is set. Setting [forceSSL]
 /// to true will redirect all http requests to https. [printRoutes] prints all
 /// the configured routes to stdout when the server starts.
-  Future run(
+  Future run(Router router,
       {int port: 8080,
       bool forceSSL: false,
       bool printRoutes: false}) async {
-    if (printRoutes) _printRoutes();
-    Server server = Server(_routerBuilder(), port: port);
+    if (printRoutes) _printRoutes(router);
+    Server server = Server(router, port: port);
     await server.start(forceSSL: forceSSL);
     return;
   }
 
-  void _printRoutes() {
-    _routerBuilder().printRoutes();
+  void _printRoutes(Router router) {
+    router.printRoutes();
     return;
   }
 }
--- lib/src/middleware.dart
+++ lib/src/middleware.dart
@@ -2,24 +2,39 @@ import 'package:arrow/src/handler.dart';
 import 'package:arrow/src/request_middleware.dart';
 import 'package:arrow/src/response_middleware.dart';
 
+/// A process performed on a [Request] before a [Handler], a [RequestMiddleware], and/or on a [Response]
+/// after a [Handler], a [ResponseMiddleware]. A [Handler] can also be provided in case of an error in
+/// the Middleware execution.
+/// 
+/// [Middleware] is added to a [Request]/[Response] [Pipeline] and can be synchronous
+/// where [useParallel] is false or asynchronous where [useParallel] is true. Middleware
+/// can also be forced to run with [useAlways] set to true; this is helpful for Middleware
+/// such as loggers, which should always run regardles of the Request/Response status.
+/// 
+/// Middleware are run in the following order:
+/// 1. Synchronous [RequestMiddleware] in the order they are added to the [Router]
+/// 2. Asynchronous [RequestMiddleware] until they are all completed
+/// 3. The request [Handler] which initiates the response
+/// 4. Asynchronous [ResponseMiddleware] until they are all completed
+/// 5. Synchronous [ResponseMiddleware] in the reverse order they are added to the [Router]
 class Middleware {
   RequestMiddleware _preProcess;
   ResponseMiddleware _postProcess;
   Handler _errorProcess;
 
   RequestMiddleware get preProcess => _preProcess;
-
   ResponseMiddleware get postProcess => _postProcess;
-
   Handler get errorProcess => _errorProcess;
 
   bool _useParallel = false;
-
-  bool get useParallel => _useParallel;
   bool _useAlways = false;
 
+  bool get useParallel => _useParallel;
   bool get useAlways => _useAlways;
 
+/// Each Middleware is provided an optional [RequestMiddleware], [ResponseMiddleware],
+/// and error [Handler]. Additional parameters [useParallel] and [useAlways] determine
+/// how the Middleware will be applied to the [Pipeline].
   Middleware(
       {RequestMiddleware pre,
       ResponseMiddleware post,
@@ -40,16 +55,16 @@ class Middleware {
   }
 }
 
-Middleware CreateMiddleware(
-    {RequestMiddleware pre,
-    ResponseMiddleware post,
-    Handler error,
-    bool useParallel: false,
-    useAlways: false}) {
-  return Middleware(
-      pre: pre,
-      post: post,
-      error: error,
-      useParallel: useParallel,
-      useAlways: useAlways);
-}
+// Middleware CreateMiddleware(
+//     {RequestMiddleware pre,
+//     ResponseMiddleware post,
+//     Handler error,
+//     bool useParallel: false,
+//     useAlways: false}) {
+//   return Middleware(
+//       pre: pre,
+//       post: post,
+//       error: error,
+//       useParallel: useParallel,
+//       useAlways: useAlways);
+// }
