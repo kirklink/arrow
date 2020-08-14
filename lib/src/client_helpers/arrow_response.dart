42bba1b4c676716c84d8df3371ae8d38978bb9f2
Kirk Linkletter <kirklinkletter@gmail.com>
2020-08-14 16:08:31 -0400

    improved client-side unwrapper

--- lib/src/client_helpers/arrow_response.dart
+++ lib/src/client_helpers/arrow_response.dart
@@ -8,11 +8,37 @@ class ArrowResponse {
   Map<String, String> _errors;
 
   ArrowResponse(Response serverResponse) {
-    final body = json.decode(serverResponse.body);
-    _ok = body['ok'];
-    _data = body['data'];
-    _errorMsg = body['errorMessage'];
-    _errors = body['errors'];
+    final body = json.decode(serverResponse.body) as Map<String, Object>;
+    
+    if (body.containsKey('ok')) {
+      _ok = body['ok'] as bool;
+    } else {
+      _ok = serverResponse.statusCode >= 200 && serverResponse.statusCode < 300;
+    }
+
+    if (body.containsKey('data')) {
+      _data = body['data'] as Map<String, Object>;
+    } else {
+      _data = const <String, Object>{};
+    }
+    
+    if (body.containsKey('errorMessage')) {
+      _errorMsg = body['errorMessage'] as String;
+    } else if (_ok){
+      _errorMsg = '';
+    } else {
+      _errorMsg = 'No body was returned from the server.';
+    }
+
+    if (body.containsKey('errors')) {
+      _errors = body['errors'] as Map<String, String>;
+    } else if (_ok) {
+      _errors = const <String, String>{};
+    } else {
+      _errors = {
+        'responseError': 'No body was return from the server.'
+      };
+    }
   }
 
   bool get ok => _ok;
