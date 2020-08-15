50e8c75ebe1eb80172c9531e18c816d02a0c04c4
Kirk Linkletter <kirklinkletter@gmail.com>
2020-08-15 06:21:48 -0400

    better client helper for browser clients

--- lib/src/client_helpers/arrow_response.dart
+++ lib/src/client_helpers/arrow_response.dart
@@ -2,53 +2,28 @@
 import 'dart:convert' show json;
 
 class ArrowResponse {
-  bool _ok;
-  Map<String, Object> _data;
-  String _errorMsg;
-  Map<String, String> _errors;
+  final bool ok;
+  final Map<String, Object> data;
+  final String errorMsg;
+  final Map<String, Object> errors;
 
-  ArrowResponse(String responseBody) {
-    Map<String, Object> body = const <String, Object>{};
-    
-    if (responseBody != null && responseBody.isNotEmpty || responseBody == null) {
-      body = json.decode(responseBody);
-    }
-    
-    if (body.containsKey('ok')) {
-      _ok = body['ok'] as bool;
-    } else {
-      _ok = false;
-    }
+  ArrowResponse._(this.ok, this.data, this.errorMsg, this.errors);
 
-    if (body.containsKey('data')) {
-      _data = body['data'] as Map<String, Object>;
+  factory ArrowResponse(String responseBody) {
+    if (responseBody == null || responseBody.isEmpty) {
+      final _ok = false;
+      final _data = const <String, dynamic>{};
+      final _errorMsg = 'No body was returned from the server.';
+      final _errors = {'serverResponse': 'No body was returned from the server.'};
+      return ArrowResponse._(_ok, _data, _errorMsg, _errors);
     } else {
-      _data = const <String, Object>{};
-    }
-    
-    if (body.containsKey('errorMessage')) {
-      _errorMsg = body['errorMessage'] as String;
-    } else if (_ok){
-      _errorMsg = '';
-    } else {
-      _errorMsg = 'No body was returned from the server.';
-    }
-
-    if (body.containsKey('errors')) {
-      _errors = body['errors'] as Map<String, String>;
-    } else if (_ok) {
-      _errors = const <String, String>{};
-    } else {
-      _errors = {
-        'responseError': 'No body was return from the server.'
-      };
+      final body = json.decode(responseBody) as Map<String, Object>;
+      final _ok = body['ok'] ?? false;
+      final _data = body['data'] ?? const <String, Object>{};
+      final _errorMsg = body['errorMessage'] ?? 'An incompatible response format was returned.';
+      final _errors = body['errors'] ?? {'serverResponse': 'An incompatible response format was returned.'};
+      return ArrowResponse._(_ok, _data, _errorMsg, _errors);
     }
   }
-
-  bool get ok => _ok;
-  Map<String, Object> get data => _data;
-  String get errorMsg => _errorMsg;
-  Map<String, String> get errors => _errors;
-
 
 }
