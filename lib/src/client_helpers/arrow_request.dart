a9c42758df6fde6e1671b1f420dfede26dc1adbf
Kirk Linkletter <kirklinkletter@gmail.com>
2020-10-24 00:14:46 -0400

    accept provided http.Client in client helpers

--- lib/src/client_helpers/arrow_request.dart
+++ lib/src/client_helpers/arrow_request.dart
@@ -1,19 +1,16 @@
 import 'dart:convert' show jsonEncode;
 import 'package:http/http.dart' as http;
 
-
 abstract class ArrowRequest {
-
   static final _sendHeaders = {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   };
 
-  static final _acceptHeaders = {
-    'Accept': 'application/json'
-  };
+  static final _acceptHeaders = {'Accept': 'application/json'};
 
-  static Map<String, String> _addAuthToken(Map<String, String> headers, String token) {
+  static Map<String, String> _addAuthToken(
+      Map<String, String> headers, String token) {
     if (token.isNotEmpty) {
       return headers..addAll({'Authorization': 'Bearer $token'});
     } else {
@@ -21,29 +18,64 @@ abstract class ArrowRequest {
     }
   }
 
-  static Future<http.Response> get(Uri uri, {String token = ''}) {
+  static Future<http.Response> get(Uri uri,
+      {String token = '', http.Client client}) {
+    if (client == null) {
+      client = http.Client();
+    }
     final headers = _addAuthToken(_acceptHeaders, token);
-    return http.get(uri, headers: headers);
+    final res = client.get(uri, headers: headers);
+    client.close();
+    return res;
   }
 
-  static Future<http.Response> post(Uri uri, {Map<String, Object> body = const <String, Object>{}, String token = ''}) async {
+  static Future<http.Response> post(Uri uri,
+      {Map<String, Object> body = const <String, Object>{},
+      String token = '',
+      http.Client client}) async {
+    if (client == null) {
+      client = http.Client();
+    }
     final headers = _addAuthToken(_sendHeaders, token);
-    return await http.post(uri, body: jsonEncode(body), headers: headers);
+    final res = client.post(uri, body: jsonEncode(body), headers: headers);
+    client.close();
+    return res;
   }
 
-  static Future<http.Response> put(Uri uri, {Map<String, Object> body = const <String, Object>{}, String token = ''}) {
+  static Future<http.Response> put(Uri uri,
+      {Map<String, Object> body = const <String, Object>{},
+      String token = '',
+      http.Client client}) {
+    if (client == null) {
+      client = http.Client();
+    }
     final headers = _addAuthToken(_sendHeaders, token);
-    return http.put(uri, body: jsonEncode(body), headers: headers);
+    final res = client.put(uri, body: jsonEncode(body), headers: headers);
+    client.close();
+    return res;
   }
 
-  static Future<http.Response> patch(Uri uri, {Map<String, Object> body = const <String, Object>{}, String token = ''}) {
+  static Future<http.Response> patch(Uri uri,
+      {Map<String, Object> body = const <String, Object>{},
+      String token = '',
+      http.Client client}) {
+    if (client == null) {
+      client = http.Client();
+    }
     final headers = _addAuthToken(_sendHeaders, token);
-    return http.patch(uri, body: jsonEncode(body), headers: headers);
+    final res = client.patch(uri, body: jsonEncode(body), headers: headers);
+    client.close();
+    return res;
   }
 
-  static Future<http.Response> delete(Uri uri, {String token = ''}) {
+  static Future<http.Response> delete(Uri uri,
+      {String token = '', http.Client client}) {
+    if (client == null) {
+      client = http.Client();
+    }
     final headers = _addAuthToken(_acceptHeaders, token);
-    return http.delete(uri, headers: headers);
+    final res = client.delete(uri, headers: headers);
+    client.close();
+    return res;
   }
-
 }
