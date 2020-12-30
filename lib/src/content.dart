e0ac7c9ecb36d4acb0b167c5d7eb32370ffc3545
Kirk Linkletter <kirklinkletter@gmail.com>
2020-12-30 19:20:37 +0000

    handle list in request body

--- lib/src/content.dart
+++ lib/src/content.dart
@@ -18,7 +18,14 @@ class JsonContent implements Content {
     if (content.trim().isEmpty) {
       _content = const {};
     } else {
-      _content = json.decode(content);
+      final c = json.decode(content);
+      if (c is Map) {
+        _content = c;
+      } else if (c is List) {
+        _content['data'] = _content;
+      } else {
+        throw FormatException('Body is not an object or list.');
+      }
     }
   }
 
