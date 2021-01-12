4c1a0a9c3b0adbfed3c20fe9431dc9e3190117df
Kirk Linkletter <kirklinkletter@gmail.com>
2021-01-12 02:36:38 +0000

    documentation

--- lib/src/handler.dart
+++ lib/src/handler.dart
@@ -3,4 +3,7 @@ import 'dart:async' show FutureOr;
 import 'request.dart';
 import 'response.dart';
 
+/// The function that executes the main logic for an endpoint. A [Handler] takes
+/// a [Request] and returns a [Response] with a data payload or relevant
+/// errors.
 typedef FutureOr<Response> Handler(Request req);
