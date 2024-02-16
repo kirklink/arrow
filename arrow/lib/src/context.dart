6bc70ee85267d4a9b1637616de982a8b31cbf4e6
Kirk Linkletter <kirklinkletter@gmail.com>
2024-02-16 23:47:49 +0000

    update context

--- arrow/lib/src/context.dart
+++ arrow/lib/src/context.dart
@@ -76,7 +76,7 @@ class Context<T> {
   }
 
   static String makeKey() {
-    final uuid = Uuid().v4(config: V4Options(_random_ints(7), CryptoRNG()));
+    final uuid = Uuid().v4(config: V4Options(_random_ints(6), CryptoRNG()));
     return uuid;
   }
 }
